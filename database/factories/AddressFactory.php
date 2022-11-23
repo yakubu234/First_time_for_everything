6d34c93bf690b1f023dd8129bade2905d3f44bfb
yakubu234 <yakubuabiola2003@gmail.com>
2022-11-23 16:23:36 +0100

    adding the relationship across all models, and generating seeders for all models

---
+++ database/factories/AddressFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Provider\Address;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Address>
+ */
+class AddressFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $max = User::count();
+        $min = ($max > 5) ? (($max -5) + 1) : 1;
+        return [
+            'user_id' => fake()->unique()->numberBetween($min, $max),
+            'address' => fake()->StreetAddress(),
+            'city' => fake()->city(),
+            'state' => fake()->stateAbbr(),
+            'zip' => Address::postcode()
+        ];
+    }
+}
---
+++ database/factories/ProfileFactory.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Profile>
+ */
+class ProfileFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $max = User::count();
+        $min = ($max > 5) ? (($max -5) + 1) : 1;
+        return [
+            'user_id' => fake()->unique()->numberBetween($min, $max),
+            'professional_summary' => 'pareag so much information to be supplied here by the user',
+            'skills' => json_encode($this->randomWords()),
+            'years_of_experience' => rand(1,10),
+            'certifications' =>json_encode($this->randomWords()),
+            'educations' => json_encode($this->randomWords()),
+        ];
+
+
+    }
+
+    public function randomWords():array
+    {
+        $input = array(
+            'random thing 1',
+            'random thing 2',
+            'random thing 3',
+            'random thing 4',
+            'random thing 5',
+            'random thing 6',
+            'random thing 7 ',
+          );
+
+          $keys = array_rand($input, 5);
+
+          $result = [
+            $input[$keys[0]],
+            $input[$keys[1]],
+            $input[$keys[2]],
+            $input[$keys[3]]
+          ];
+
+          shuffle($result);
+          return $result;
+    }
+}
